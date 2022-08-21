8f034c5d6005ca6513f8ca33a2bebbb2864311f0
OspanovaVita <vitaospanova@ukr.net>
2022-08-21 13:30:49 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchImages } from './js/fetch-images';
+import { fetchImages, PER_PAGE } from './js/fetch-images';
 import { renderGallery } from './js/render-gallery';
 
 import Notiflix from 'notiflix';
@@ -11,91 +11,78 @@ const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.btn-load-more');
 let query = '';
 let page = 1;
-let simpleLightBox;
-const perPage = 40;
+const lightBox = new SimpleLightbox('.gallery a');
 
 searchForm.addEventListener('submit', onSearchForm);
-// loadMoreBtn.addEventListener('click', onLoadMoreBtn);
-window.addEventListener('scroll', onScroll);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+// window.addEventListener('scroll', onScroll);
 
 function onSearchForm(e) {
     e.preventDefault();
-    window.scrollTo({ top: 0 });
+    // window.scrollTo({ top: 0 });
     page = 1; 
     query = e.currentTarget.searchQuery.value.trim();
+    
+  if (query === '') {
+      Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+      return;
+    }
     gallery.innerHTML = '';
     loadMoreBtn.classList.add('is-hidden');
-
-  if (query === '') {
-      alertNoEmptySearch();
-      return;
-  }
-    fetchImages(query, page, perPage)
+    fetchImages(query, page)
         .then(({ data }) => {
             if (data.totalHits === 0) {
-                alertNoImagesFound();
+                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
             } else {
                 renderGallery(data.hits);
-                simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-                alertImagesFound(data);
+                lightBox.refresh();
+                Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
 
-                if (data.totalHits > perPage) {
-                    // loadMoreBtn.classList.remove('is-hidden');
+                if (data.totalHits >= PER_PAGE) {                
+                    loadMoreBtn.classList.remove('is-hidden');
                 }
             }
         })
         .catch(error => console.log(error));
 }
 
-// function onLoadMoreBtn() {
-//     page += 1; 
-//     simpleLightBox.destroy()
-//     fetchImages(query, page, perPage)
-//         .then(({ data }) => {
-//             renderGallery(data.hits);
-//             simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+function onLoadMoreBtn() {
+    page += 1; 
+    fetchImages(query, page)
+        .then(({ data }) => {
+            renderGallery(data.hits);
+            lightBox.refresh();
 
-//             const totalPages = Math.ceil(data.totalHits / perPage);
+            const totalPages = Math.ceil(data.totalHits / PER_PAGE);
 
-//             if (page > totalPages) {
-//                 loadMoreBtn.classList.add('is-hidden');
-//                 alertEndOfSearch();
-//             }
-//         })
-//         .catch(error => console.log(error));
-// }
-
-function alertImagesFound(data) {
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+            if (page === totalPages) {
+                loadMoreBtn.classList.add('is-hidden');
+                Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+            }
+        })
+        .catch(error => console.log(error));
 }
-
-function alertNoEmptySearch() {
-    Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
-}
-
-function alertNoImagesFound() {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-}
-
-function alertEndOfSearch() {
-    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-}
-
 
 // Endless scroll
 
 // и закоментировать 43 строку 
-function onScroll() {
-    const documentRect = document.documentElement.getBoundingClientRect();
-    if (documentRect.bottom < document.documentElement.clientHeight + 150) {                
-        page += 1; 
-        simpleLightBox.destroy()
-        fetchImages(query, page, perPage)
-            .then(({ data }) => {
-                renderGallery(data.hits);
-                simpleLightBox = new SimpleLightbox('.gallery a').refresh();                
-            })
-            .catch(error => console.log(error));
-    }    
-}
+// function onScroll() {
+//     const documentRect = document.documentElement.getBoundingClientRect();
+//     if (documentRect.bottom < document.documentElement.clientHeight + 150) {                
+//         page += 1; 
+//         // lightBox.destroy()
+//         fetchImages(query, page)
+//             .then(({ data }) => {
+//                 renderGallery(data.hits);
+//                 lightBox.refresh(); 
+//                 const totalPages = Math.ceil(data.totalHits / PER_PAGE);
+
+//             if (page === totalPages) {
+//                 // loadMoreBtn.classList.add('is-hidden');
+//                 Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+//             }
+//             })
+//             .catch(error => console.log(error));
+//     }    
+// }
 
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
-export { fetchImages };
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '29360332-a92f3069e6903f60a66cd9589';
-const URLSearchParams = 'image_type=photo&orientation=horizontal&safesearch=true';
+const URL_SEARCH_PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
+const PER_PAGE = 40;
 
-async function fetchImages(query, page, perPage) {
-    const response = await axios.get(
-        `${BASE_URL}?key=${KEY}&${URLSearchParams}&q=${query}&page=${page}&per_page=${perPage}`
-    );
+async function fetchImages(query, page, PER_PAGE) {
+     
+     const response = await axios.get(
+        `${BASE_URL}?key=${KEY}&${URL_SEARCH_PARAMS}&q=${query}&page=${page}&per_page=${PER_PAGE}`
+     );
     return response;
 }
+export { fetchImages, PER_PAGE };
